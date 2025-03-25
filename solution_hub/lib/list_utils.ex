2522ab8d9fc64efc517ad9df11a87987ae669875
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-03-24 21:05:33 -0300

    feat: solve exercises in StringUtils and ListUtils

--- solution_hub/lib/list_utils.ex
+++ solution_hub/lib/list_utils.ex
@@ -1,9 +1,33 @@
 defmodule ListUtils do
 
-  #6
+  @moduledoc """
+  6 - Implemente uma versão da função map/2 para aplicar uma função a todos os
+  elementos de uma lista.
+  """
   def my_map([], _func), do: []
   def my_map([head | tail], func) do
     [func.(head) | my_map(tail, func)]
+  end
+
+  @moduledoc """
+  7 - Transforme uma lista de números, dobrando cada número, filtrando os que
+  são maiores que 10 e somando os valores finais.
+  """
+  def process_list(list) do
+    list
+      |> Enum.map(fn x -> x * 2 end)
+      |> Enum.filter(fn x -> x > 10 end)
+      |> Enum.reduce(0, fn x, acc -> x + acc end)
+  end
+
+  @moduledoc """
+  9 - Recrie a função Enum.each/2, que recebe uma lista e uma função e executa essa função
+  para cada elemento.
+  """
+  def my_each([], _func), do: :ok
+  def my_each([head | tail], func) do
+    func.(head)
+    my_each(tail, func)
   end
 
 end
--- solution_hub/lib/string_utils.ex
+++ solution_hub/lib/string_utils.ex
@@ -15,4 +15,48 @@ defmodule StringUtils do
       |> length()
   end
 
+  @moduledoc """
+  8 - Crie uma função que recebe uma tupla representando uma pessoa ({:ok, nome, idade}
+  ou {:error, motivo}) e retorne uma mensagem apropriada.
+  """
+  def check_person({:ok, name, age}) when age >= 18 do
+    "#{name} is an adult."
+  end
+  def check_person({:ok, name, _age}) do
+    "#{name} is underage."
+  end
+  def check_person ({:error, motivo}) do
+    "Error: #{motivo}"
+  end
+
+  @moduledoc """
+  10 - Crie uma função que recebe um mapa de produtos e
+  retorna um novo mapa apenas com os produtos mais caros que R$50.
+  """
+  def filter_products(products) do
+    products
+      |> Enum.filter(fn {_product, price} -> price > 50 end)
+      |> Enum.into(%{}) #transforma um
+  end
+
+  @moduledoc """
+  11 - Dado um conjunto de tuplas {produto, quantidade},
+  calcule a soma total das quantidades usando Enum.reduce/3 e pipe operator
+  """
+  def total_items(items) do
+    items
+      |> Enum.reduce(0, fn {_name, number}, acc -> acc + number end)
+  end
+
+  @moduledoc """
+  12 - Crie uma função que recebe uma string no formato "Nome,Idade,Cidade" e retorna um mapa
+  """
+  def parse(str) do
+    str
+    |> String.split(",")
+    |> case do # permite fazer pattern matching diretamente no fluxo de transformação de dados
+      [nome, idade, cidade] -> %{nome: nome, idade: String.to_integer(idade), cidade: cidade}
+      _ -> %{erro: "Formato inválido"}
+    end
+  end
 end

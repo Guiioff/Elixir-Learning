81cc5405caaf59eafc0eb700ac71ce6031de3409
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-03-28 20:39:38 -0300

    feat: update game state management

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -16,7 +16,7 @@ defmodule ExMon do
     # computer = create_player("Robotinik", :punch, :kick, :heal)
     # Game.start(computer, player)
 
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
@@ -31,6 +31,8 @@ defmodule ExMon do
       :move_heal -> "realiza_cura"
       move -> Actions.attack(move)
     end
+
+    Status.print_round_message(Game.info())
   end
 
 end
